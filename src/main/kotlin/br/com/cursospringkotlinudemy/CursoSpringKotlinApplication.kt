eca3d7bc9d852e7e34fefc302512e4e456241fb8
Levi Mendes <levi.mendes@foursys.com.br>
2022-01-16 17:24:19 -0300

    first commit

---
+++ src/main/kotlin/br/com/cursospringkotlinudemy/CursoSpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cursospringkotlinudemy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CursoSpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CursoSpringKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/cursospringkotlinudemy/CursoSpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.cursospringkotlinudemy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursoSpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

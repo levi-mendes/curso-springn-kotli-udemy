51df97005a6236a20a2fc0fbc95b84292f826f11
Levi Mendes <levi.mendes@foursys.com.br>
2022-01-16 19:03:50 -0300

    primeira rota

--- src/main/kotlin/br/com/cursospringkotlinudemy/controller/CustomerController.kt
+++ src/main/kotlin/br/com/cursospringkotlinudemy/controller/CustomerController.kt
@@ -1,11 +1,38 @@
 package br.com.cursospringkotlinudemy.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import br.com.cursospringkotlinudemy.controller.request.PostCustomerRequest
+import br.com.cursospringkotlinudemy.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("customers")
 class CustomerController {
 
+    var customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun helloWorld() = "Hello World"
+    fun getAll(): List<CustomerModel> {
+        return customers
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        customers.add(CustomerModel(id(), customer.name, customer.email))
+    }
+
+    fun id(): String {
+        return if (customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id.toInt().plus(1)
+        }.toString()
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.first { it.id == id }
+    }
+
 }
